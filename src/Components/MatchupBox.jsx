cf4ab9c3951463228611e3816fa1ecea3ebf015b
MichaelBHerman <michaelbherman@gmail.com>
2022-01-15 16:22:09 -0700

    fixed error of afc mvp not appearing

--- src/Components/MatchupBox.jsx
+++ src/Components/MatchupBox.jsx
@@ -61,12 +61,6 @@ switch (nfcTeam) {
         break;    
 }
 let mvp;
-
-
-
-let mvp1;
-
-
 let timage;
 
 switch (afcTeam) {
@@ -92,14 +86,14 @@ switch (afcTeam) {
         timage = titans
         break;    
   }
-  function getScore(max) {
+  function getScore(min, max) {
     return Math.floor(Math.random() * max);
 }
     let nfcScore = 0;
     let afcScore = 0;
-    nfcScore = getScore(55);
+    nfcScore = getScore(2, 55);
         console.log(nfcScore);
-    afcScore = getScore(55);
+    afcScore = getScore(2, 55);
         console.log(afcScore);
   
 
@@ -136,35 +130,35 @@ switch (afcTeam) {
             break;    
     }
   }
-  else{
+  else if (afcScore > nfcScore) {
     switch (afcTeam) {
         case 'Raiders':
             let players7 =["Derek Carr", "Josh Jacobs", "DeSean Jackson", "Hunter Renfrow", "Denzel Perryman", "Darren Waller"]
-            mvp1 = players7[Math.floor(Math.random()*items.length)];
+            mvp = players7[Math.floor(Math.random()*items.length)];
             break;
         case 'Bengals':
             let players8 = ["Joe Burrow", "Joe Mixon", "Ja'Marr Chase", "Tee Higgins", "Trey Hendrickson"]
-            mvp1 = players8[Math.floor(Math.random()*items.length)];
+            mvp = players8[Math.floor(Math.random()*items.length)];
             break;
         case 'Patriots':
             let players9 = ["Mac Jones", "Damien Harris", "Jakobi Meyers", "Hunter Henry", "Matthew Judon"]
-            mvp1 = players9[Math.floor(Math.random()*items.length)];
+            mvp = players9[Math.floor(Math.random()*items.length)];
             break;   
         case 'Bills':
             let players10 = ["Josh Allen", "Devin Singletary", "Stefon Diggs", "Tremaine Edmunds", "Micah Hyde"]
-            mvp1 = players10[Math.floor(Math.random()*items.length)];
+            mvp = players10[Math.floor(Math.random()*items.length)];
             break;
         case 'Steelers':
             let players11 = ["Ben Roethlisberger", "Najee Harris", "Diontae Johnson", "Minkah Fitzpatrick", "T.J. Watt"]
-            mvp1 = players11[Math.floor(Math.random()*items.length)];
+            mvp = players11[Math.floor(Math.random()*items.length)];
             break;
         case 'Chiefs':
             let players12 =["Patrick Mahomes", "Tyreek Hill", "Clyde Edwards-Helaire", "Travis Kelce", "Tyrann Mathieu"]
-            mvp1 = players12[Math.floor(Math.random()*items.length)];
+            mvp = players12[Math.floor(Math.random()*items.length)];
             break;
         case 'Titans':
             let players13 =["Ryan Tannehill", "Derrick Henry", "Harold Landry III", "A.J. Brown"]
-            mvp1 = players13[Math.floor(Math.random()*items.length)];
+            mvp = players13[Math.floor(Math.random()*items.length)];
             break;    
       }
   }
